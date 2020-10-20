5ff06304305ccf4b033eb87f3594017d09fccb98
saint <saint@DESKTOP-LIIG4VH>
2020-10-20 22:31:43 +0900

    JSONを利用したデータの取得

--- WebScrapingForNarou/Scraping.cs
+++ WebScrapingForNarou/Scraping.cs
@@ -8,6 +8,8 @@ using System.Net;
 using System.Text.RegularExpressions;
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections;
 
 namespace WebScrapingForNarou
 {
@@ -45,132 +47,20 @@ namespace WebScrapingForNarou
             return ncode;
         }
 
-        ///// <summary>
-        ///// Nコード(書籍コード)から小説情報URLを取得
-        ///// </summary>
-        ///// <param name="ncode">Nコード(書籍コード)</param>
-        ///// <returns>URL(アドレス)</returns>
-        //private static string GetInfoUrl(string ncode)
-        //{
-        //    return "https://ncode.syosetu.com/novelview/infotop/ncode/" + ncode + "/";
-        //}
-
-        ///// <summary>
-        ///// 引数urlにアクセスした際に取得できるHTMLを返します。
-        ///// </summary>
-        ///// <param name="url">URL(アドレス)</param>
-        ///// <returns>取得したHTML</returns>
-        //public string GetHtml(string url)
-        //{
-        //    //// 指定されたURLに対してのRequestを作成します。
-        //    //var req = (HttpWebRequest)WebRequest.Create(url);
-
-        //    //// html取得文字列
-        //    //string html = "";
-
-        //    //// 指定したURLに対してReqestを投げてResponseを取得します。
-        //    //using (var res = (HttpWebResponse)req.GetResponse())
-        //    //using (var resStream = res.GetResponseStream())
-        //    //// 取得した文字列をUTF8でエンコードします。
-        //    //using (var sr = new StreamReader(resStream, Encoding.UTF8))
-        //    //{
-        //    //    // HTMLを取得する。
-        //    //    html = sr.ReadToEnd();
-        //    //}
-
-        //    WebClient wc = new WebClient();
-        //    string html = "";
-        //    try
-        //    {
-        //        html = wc.DownloadString(url);
-        //    }
-        //    catch (WebException exc)
-        //    {
-        //        html = exc.Message;
-        //    }
-
-        //    return html;
-        //}
-
-        ///// <summary>
-        ///// 正規化表現を使用してHTMLからタイトルを取得します。
-        ///// </summary>
-        ///// <param name="html">HTML文字列</param>
-        ///// <returns>HTML文字列から取得したタイトル</returns>
-        //public string GetTitle(string html)
-        //{
-
-        //    // 正規化表現
-        //    // 大文字小文字区別なし       : RegexOptions.IgnoreCase
-        //    // 「.」を改行にも適応する設定: RegexOptions.Singleline
-        //    // ?<title>により"title"というグループ名をつけている
-        //    // .*?により任意の文字の並びを表している
-        //    var reg = new Regex(@"<title>(?<title>.*?)</title>",
-        //                  RegexOptions.IgnoreCase | RegexOptions.Singleline);
-
-        //    // html文字列内から条件にマッチしたデータを抜き取ります。
-        //    var m = reg.Match(html);
-
-        //    // 条件にマッチした文字列内からKey("title")にマッチした値を抜き取ります。
-        //    return m.Groups["title"].Value;
-
-        //}
-
-        ///// <summary>
-        ///// 正規化表現を使用してHTMLから話数を取得します。
-        ///// </summary>
-        ///// <param name="html">HTML文字列</param>
-        ///// <returns>話数</returns>
-        //public string GetNumOfStory(string html)
-        //{
-
-        //    // 正規化表現
-        //    // 大文字小文字区別なし       : RegexOptions.IgnoreCase
-        //    // 「.」を改行にも適応する設定: RegexOptions.Singleline
-        //    var reg = new Regex(@"全(?<numOfStory>.*?)部分");
-
-        //    // html文字列内から条件にマッチしたデータを抜き取ります。
-        //    var m = reg.Match(html);
-
-        //    // 条件にマッチした文字列内からKey("numOfStory")にマッチした値を抜き取ります。
-        //    return m.Groups["numOfStory"].Value;
-
-        //}
-
-        ///// <summary>
-        ///// 正規化表現を使用してHTMLから文字数を取得します。
-        ///// </summary>
-        ///// <param name="html">HTML文字列</param>
-        ///// <returns>文字数</returns>
-        //public string GetNumOfWord(string html)
-        //{
-
-        //    // 正規化表現
-        //    // 大文字小文字区別なし       : RegexOptions.IgnoreCase
-        //    // 「.」を改行にも適応する設定: RegexOptions.Singleline
-        //    // ?<title>により"title"というグループ名をつけている
-        //    // .*?により任意の文字の並びを表している
-        //    var reg = new Regex(@"<td>(?<numOfWord>.*?文字)</td>",
-        //                  RegexOptions.IgnoreCase | RegexOptions.Singleline);
-
-        //    // html文字列内から条件にマッチしたデータを抜き取ります。
-        //    var m = reg.Match(html);
-
-        //    // 条件にマッチした文字列内からKey("numOfWord")にマッチした値を抜き取ります。
-        //    return m.Groups["numOfWord"].Value;
-
-        //}
-
-
         //小説家になろうAPIについてのページ
         //https://dev.syosetu.com/man/api/
 
+        //ofパラメータ
+        //of=t-n など
         //t:title	小説名
         //n:ncode	Nコード
         //w:writer	作者名
         //ga:general_all_no	全掲載部分数です。短編の場合は1です。
         //l:length	小説文字数です。スペースや改行は文字数としてカウントしません。
         //ti:time	読了時間(分単位)です。読了時間は小説文字数÷500を切り上げした数値です。
+
+        //outパラメータ
+        //out=json JSON形式にする
 
         /// <summary>
         /// Nコード(書籍コード)から小説情報APIURLを取得
@@ -179,7 +69,7 @@ namespace WebScrapingForNarou
         /// <returns>小説情報APIURL</returns>
         private static string GetApiUrl(string ncode)
         {
-            return "https://api.syosetu.com/novelapi/api/?of=t-n-w-ga-l-ti&ncode=" + ncode;
+            return "https://api.syosetu.com/novelapi/api/?out=json&of=t-n-w-ga-l-ti&ncode=" + ncode;
         }
 
         /// <summary>
@@ -187,25 +77,29 @@ namespace WebScrapingForNarou
         /// </summary>
         /// <param name="apiUrl">APIURL</param>
         /// <returns>取得したAPIData</returns>
-        public APIDataNarou GetApiData(string apiUrl)
+        public JArray GetApiData(string apiUrl)
         {
 
             WebClient wc = new WebClient();
             wc.Headers.Add("user-agent", "Get Narou Info");
-            APIDataNarou apiData = new APIDataNarou();
+            JArray apiData = new JArray();
             try
             {
                 string data = wc.DownloadString(apiUrl);
 
-                //参考URL : https://www.atmarkit.co.jp/ait/articles/1709/13/news018.html
-                string output = JsonConvert.SerializeObject(data);
-                apiData = JsonConvert.DeserializeObject<APIDataNarou>(output);
+                //まずはバイト配列に変換する
+                byte[] bytes = Encoding.UTF8.GetBytes(data);
+
+                //バイト配列をUTF8の文字コードとしてStringに変換する
+                string stringUtf8 = System.Text.Encoding.UTF8.GetString(bytes);
+
+                // デシリアライズ
+                apiData = JArray.Parse(stringUtf8);
             }
             catch (WebException exc)
             {
-                apiData.Comment = exc.Message;
+                apiData.Add("error : " + exc.Message);
             }
-
             return apiData;
             
         }
@@ -220,32 +114,34 @@ namespace WebScrapingForNarou
 
             string nCode = GetNCode(url);
             string apiUrl = GetApiUrl(nCode);
-            APIDataNarou apiData = GetApiData(apiUrl);
-            string infoStory = "";
-            if (apiData.Comment == null)
+            JArray apiData = new JArray();
+            if (nCode == "")
             {
-                string title = apiData.Data["t"];
-                string writer = apiData.Data["w"];
-                string numOfStory = apiData.Data["ga"];
-                string numOfWord = apiData.Data["l"];
-                string time = apiData.Data["ti"];
-                //書籍コード, 作者名 , 小説名  , 話数 , 文字数 , 読了時間
-                infoStory = nCode + "\t" + writer + "\t" + title + "\t" + numOfStory + "\t" + numOfWord + "\t" + time;
+                apiData.Add("error : 書籍コードエラー");
             }
             else
             {
-                infoStory = apiData.Comment;
+                apiData = GetApiData(apiUrl);
+            }
+            string infoStory = "";
+            JToken jDataLast = apiData.Last;
+            JTokenType jTypeLast = jDataLast.Type;
+            if (apiData.Count >= 2 && jTypeLast != JTokenType.String)
+            {
+                string title      = (string)apiData.Last["title"];
+                string writer     = (string)apiData.Last["writer"];
+                string numOfStory = (string)apiData.Last["general_all_no"];
+                string numOfWord  = (string)apiData.Last["length"];
+                string time       = (string)apiData.Last["time"];
+                //書籍コード, 作者名 , 小説名  , 話数 , 文字数 , 読了時間
+                infoStory = nCode + "\t" + writer + "\t" + title + "\t" + numOfStory + "\t" + numOfWord + "\t" + time + "\r\n";
+            }
+            else
+            {
+                infoStory = (string)apiData.ToString();
             }
             return infoStory;
         }
-    }
-
-    public class APIDataNarou
-    {
-        // 先のSampleDataと比べて、DescriptionとUpdateDateがない
-        public Dictionary<string, string> Data { get; set; }
-        // Commentが余分にある
-        public string Comment { get; set; }
     }
 
 }
